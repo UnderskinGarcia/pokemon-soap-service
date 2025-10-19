bfea1f352a5e8ad06e7555d652d70690bb248ce8
UnderskinGarcia <raul.e.garciacabrera@gmaill.com>
2025-10-19 15:06:17 -0600

    add BadRequestException

---
+++ src/main/java/com/bankaya/pokemon/domain/exception/BadRequestException.java
@@ -0,0 +1,15 @@
+package com.bankaya.pokemon.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Domain Exception - Bad Request
+ * Thrown when the request parameters are invalid
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
