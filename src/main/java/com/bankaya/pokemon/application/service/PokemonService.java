21ea7f06c5d74e4c7500078f1bb23305bd1c25fe
UnderskinGarcia <raul.e.garciacabrera@gmaill.com>
2025-10-19 23:11:41 -0600

    Implement PokemonEndpoint as SOAP adapter with handlers for abilities, base experience, held items, id, name, and location encounters. Configure Spring Web Services with WSDL generation and message dispatcher servlet.

--- src/main/java/com/bankaya/pokemon/application/service/PokemonService.java
+++ src/main/java/com/bankaya/pokemon/application/service/PokemonService.java
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Service;
 import com.bankaya.pokemon.domain.model.Pokemon;
 import com.bankaya.pokemon.domain.ports.GetPokemonUseCase;
 import com.bankaya.pokemon.domain.ports.PokemonApiPort;
+import com.bankaya.pokemon.soap.Ability;
+import com.bankaya.pokemon.soap.GetPokemonAbilitiesResponse;
+import com.bankaya.pokemon.soap.GetPokemonBaseExperienceResponse;
+import com.bankaya.pokemon.soap.GetPokemonHeldItemsResponse;
+import com.bankaya.pokemon.soap.GetPokemonIdResponse;
+import com.bankaya.pokemon.soap.GetPokemonLocationAreaEncountersResponse;
+import com.bankaya.pokemon.soap.GetPokemonNameResponse;
+import com.bankaya.pokemon.soap.HeldItem;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -25,6 +33,88 @@ public class PokemonService implements GetPokemonUseCase {
     public Pokemon getPokemonByName(String pokemonName) {
         log.info("Fetching Pokemon by name: {}", pokemonName);
         return pokemonApiPort.fetchPokemonByName(pokemonName);
+    }
+
+    @Override
+    public GetPokemonAbilitiesResponse getPokemonAbilities(String pokemonName) {
+        Pokemon pokemon = getPokemonByName(pokemonName);
+
+        GetPokemonAbilitiesResponse response = new GetPokemonAbilitiesResponse();
+
+        if (pokemon.abilities() != null) {
+            pokemon.abilities().forEach(ability -> {
+                Ability soapAbility = new Ability();
+                soapAbility.setName(ability.name());
+                soapAbility.setUrl(ability.url());
+                soapAbility.setIsHidden(ability.isHidden());
+                soapAbility.setSlot(ability.slot());
+                response.getAbilities().add(soapAbility);
+            });
+        }
+        return response;
+    }
+
+    @Override public GetPokemonBaseExperienceResponse getPokemonBaseExperienceResponse(String pokemonName) {
+        Pokemon pokemon = getPokemonByName(pokemonName);
+
+        GetPokemonBaseExperienceResponse response = new GetPokemonBaseExperienceResponse();
+        response.setBaseExperience(pokemon.baseExperience());
+        return response;
+    }
+
+    @Override public GetPokemonHeldItemsResponse getPokemonHeldItems(String pokemonName) {
+        log.info("SOAP Request - Get Pokemon Held Items: {}", pokemonName);
+
+        Pokemon pokemon = getPokemonByName(pokemonName);
+
+        GetPokemonHeldItemsResponse response = new GetPokemonHeldItemsResponse();
+
+        if (pokemon.heldItems() != null) {
+            pokemon.heldItems().forEach(item -> {
+                HeldItem soapItem = new HeldItem();
+                soapItem.setName(item.name());
+                soapItem.setUrl(item.url());
+                response.getHeldItems().add(soapItem);
+            });
+        }
+
+        return response;
+    }
+
+    @Override
+    public GetPokemonIdResponse getPokemonId(String pokemonName) {
+        log.info("SOAP Request - Get Pokemon ID: {}", pokemonName);
+
+        Pokemon pokemon = getPokemonByName(pokemonName);
+
+        GetPokemonIdResponse response = new GetPokemonIdResponse();
+        response.setId(pokemon.id());
+
+        return response;
+    }
+
+    @Override
+    public GetPokemonNameResponse getPokemonName(String pokemonName) {
+        log.info("SOAP Request - Get Pokemon Name: {}", pokemonName);
+
+        Pokemon pokemon = getPokemonByName(pokemonName);
+
+        GetPokemonNameResponse response = new GetPokemonNameResponse();
+        response.setName(pokemon.name());
+
+        return response;
+    }
+
+    @Override
+    public GetPokemonLocationAreaEncountersResponse getPokemonLocationAreaEncounters(String pokemonName) {
+        log.info("SOAP Request - Get Pokemon Location Area Encounters: {}", pokemonName);
+
+        Pokemon pokemon = getPokemonByName(pokemonName);
+
+        GetPokemonLocationAreaEncountersResponse response = new GetPokemonLocationAreaEncountersResponse();
+        response.setLocationAreaEncounters(pokemon.locationAreaEncounters());
+
+        return response;
     }
 
 }
---
+++ src/main/java/com/bankaya/pokemon/infrastructure/adapter/soap/PokemonEndpoint.java
@@ -0,0 +1,85 @@
+package com.bankaya.pokemon.infrastructure.adapter.soap;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.bankaya.pokemon.application.service.PokemonService;
+import com.bankaya.pokemon.domain.model.Pokemon;
+import com.bankaya.pokemon.soap.Ability;
+import com.bankaya.pokemon.soap.GetPokemonAbilitiesResponse;
+import com.bankaya.pokemon.soap.GetPokemonBaseExperienceResponse;
+import com.bankaya.pokemon.soap.GetPokemonHeldItemsResponse;
+import com.bankaya.pokemon.soap.GetPokemonIdResponse;
+import com.bankaya.pokemon.soap.GetPokemonLocationAreaEncountersResponse;
+import com.bankaya.pokemon.soap.GetPokemonNameResponse;
+import com.bankaya.pokemon.soap.HeldItem;
+import com.bankaya.pokemon.soap.PokemonNameRequest;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * SOAP Endpoint for Pokemon Web Service
+ * Input Adapter in Hexagonal Architecture
+ */
+@Log4j2
+@Endpoint
+@RequiredArgsConstructor
+public class PokemonEndpoint {
+
+    private static final String NAMESPACE_URI = "http://bankaya.com/pokemon/soap";
+
+    private final PokemonService pokemonService;
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonAbilitiesRequest")
+    @ResponsePayload
+    public GetPokemonAbilitiesResponse getPokemonAbilities(@RequestPayload PokemonNameRequest request) {
+        log.info("SOAP Request - Get Pokemon Abilities: {}", request.getName());
+
+        return pokemonService.getPokemonAbilities(request.getName());
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonBaseExperienceRequest")
+    @ResponsePayload
+    public GetPokemonBaseExperienceResponse getPokemonBaseExperience(
+            @RequestPayload PokemonNameRequest request) {
+        log.info("SOAP Request - Get Pokemon Base Experience: {}", request.getName());
+
+        return pokemonService.getPokemonBaseExperienceResponse(request.getName());
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonHeldItemsRequest")
+    @ResponsePayload
+    public GetPokemonHeldItemsResponse getPokemonHeldItems(@RequestPayload PokemonNameRequest request) {
+        log.info("SOAP Request - Get Pokemon Held Items: {}", request.getName());
+
+        return pokemonService.getPokemonHeldItems(request.getName());
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonIdRequest")
+    @ResponsePayload
+    public GetPokemonIdResponse getPokemonId(@RequestPayload PokemonNameRequest request) {
+        log.info("SOAP Request - Get Pokemon ID: {}", request.getName());
+
+        return pokemonService.getPokemonId(request.getName());
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonNameRequest")
+    @ResponsePayload
+    public GetPokemonNameResponse getPokemonName(@RequestPayload PokemonNameRequest request) {
+        log.info("SOAP Request - Get Pokemon Name: {}", request.getName());
+
+        return pokemonService.getPokemonName(request.getName());
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "GetPokemonLocationAreaEncountersRequest")
+    @ResponsePayload
+    public GetPokemonLocationAreaEncountersResponse getPokemonLocationAreaEncounters(
+            @RequestPayload PokemonNameRequest request) {
+        log.info("SOAP Request - Get Pokemon Location Area Encounters: {}", request.getName());
+
+        return pokemonService.getPokemonLocationAreaEncounters(request.getName());
+    }
+}
