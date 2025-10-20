991fabe1ea6738c4cc709292c85cbbfe4ac8f295
UnderskinGarcia <raul.e.garciacabrera@gmaill.com>
2025-10-19 23:04:45 -0600

    Add basic ws configuration:
     - improve log4j2.properties
     - add specific logs
     - add pokemon.xsd to ws requests
     - add gradle xjc, wsdl4j and jaxb dependencies
     - xjc generated classes config

--- src/main/java/com/bankaya/pokemon/PokemonSoapServiceApplication.java
+++ src/main/java/com/bankaya/pokemon/PokemonSoapServiceApplication.java
@@ -2,7 +2,9 @@ package com.bankaya.pokemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ws.config.annotation.EnableWs;
 
+@EnableWs
 @SpringBootApplication
 public class PokemonSoapServiceApplication {
 
