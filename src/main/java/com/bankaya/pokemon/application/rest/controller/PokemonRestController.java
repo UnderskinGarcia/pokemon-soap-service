93cb6910d05559ed44ae6691755e8cbae5438ade
maizdemicorazon <maizdemicorazn@gmail.com>
2025-10-21 17:47:15 -0600

    remove api package

--- src/main/java/com/bankaya/pokemon/application/rest/controller/PokemonRestController.java
+++ src/main/java/com/bankaya/pokemon/application/rest/controller/PokemonRestController.java
@@ -1,4 +1,4 @@
-package com.bankaya.pokemon.application.api.controller;
+package com.bankaya.pokemon.application.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
