a6156c02913d99d29766fb02c0c427e3f8ef3f2b
maizdemicorazon <maizdemicorazn@gmail.com>
2025-10-21 17:52:32 -0600

    Moving annotation to WsConfigurer makes more sense

--- src/main/java/com/bankaya/pokemon/PokemonSoapServiceApplication.java
+++ src/main/java/com/bankaya/pokemon/PokemonSoapServiceApplication.java
@@ -3,9 +3,7 @@ package com.bankaya.pokemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.ws.config.annotation.EnableWs;
 
-@EnableWs
 @EnableCaching
 @SpringBootApplication
 public class PokemonSoapServiceApplication {
